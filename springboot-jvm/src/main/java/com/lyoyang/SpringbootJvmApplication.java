22d47412d1b1a7cb98bffdf237ce74c79ad404c1
yangbing <yangbing0828@qq.com>
2019-04-09 23:01:46 +0800

    jvm

---
+++ springboot-jvm/src/main/java/com/lyoyang/SpringbootJvmApplication.java
@@ -0,0 +1,13 @@
+package com.lyoyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJvmApplication.class, args);
+    }
+
+}
