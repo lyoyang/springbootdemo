3f32bb61db38f7dbafd45b30333425c798905ee8
yangbing <yangbing0828@qq.com>
2019-05-05 23:42:27 +0800

    springboot for dubbo

---
+++ boot-dubbo-provider/src/main/java/com/lyoyang/ProviderStarter.java
@@ -0,0 +1,17 @@
+package com.lyoyang;
+
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableDubbo
+@EnableHystrix
+public class ProviderStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderStarter.class, args);
+    }
+}
