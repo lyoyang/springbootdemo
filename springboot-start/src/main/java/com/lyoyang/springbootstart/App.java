f49d8a42f77143d538ba59ab0f1a1aa2c7b81f99
yangbing <yangbing0828@qq.com>
2019-06-19 23:47:12 +0800

    swagger

--- springboot-start/src/main/java/com/lyoyang/springbootstart/App.java
+++ springboot-start/src/main/java/com/lyoyang/springbootstart/App.java
@@ -8,12 +8,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan(basePackages = {"com.lyoyang.springbootstart.controller","com.lyoyang.springbootstart.service","com.lyoyang.springbootstart.entity","com.lyoyang.springbootstart.datasource","com.lyoyang.springbootstart.aspect"})
 @EnableAutoConfiguration
 @MapperScan("com.lyoyang.springbootstart.mapper")
 @SpringBootConfiguration
 @EnableConfigurationProperties
+@EnableSwagger2
 public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
