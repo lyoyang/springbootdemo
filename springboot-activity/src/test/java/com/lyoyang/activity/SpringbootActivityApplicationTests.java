dcbb4560ecdabea45e5465aa84811ab6ff6a42c2
杨兵 <yangbing@ipaynow.cn>
2020-04-24 19:15:23 +0800

    'activity'

---
+++ springboot-activity/src/test/java/com/lyoyang/activity/SpringbootActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyoyang.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
