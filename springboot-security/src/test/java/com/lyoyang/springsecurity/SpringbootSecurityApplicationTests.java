5a53f86c714e510118a1b92155db60a5e0527fb6
杨兵 <yangbing@ipaynow.cn>
2020-05-19 18:17:33 +0800

    test

--- springboot-security/src/test/java/com/lyoyang/springsecurity/SpringbootSecurityApplicationTests.java
+++ springboot-security/src/test/java/com/lyoyang/springsecurity/SpringbootSecurityApplicationTests.java
@@ -1,16 +1,37 @@
 package com.lyoyang.springsecurity;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SecurityApplication.class)
 public class SpringbootSecurityApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mvc;
+
+	@BeforeEach
+	public void setup(WebApplicationContext webApplicationContext) {
+		mvc = MockMvcBuilders
+				.webAppContextSetup(webApplicationContext)
+				.apply(springSecurity())
+				.build();
 	}
+
+	@Test
+	public void testSign() throws Exception {
+		ResultActions resultActions = mvc.perform(formLogin("/doLogin").user("yangbing").password("12345"));
+		System.out.println(resultActions);
+	}
+
 
 }
